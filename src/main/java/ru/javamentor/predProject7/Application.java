c2217a386d0fe26445e151218aafa06dea19c6b7
nick <gnn@mm.st>
2019-12-05 11:49:23 +0300

    add app

--- src/main/java/ru/javamentor/predProject7/Application.java
+++ src/main/java/ru/javamentor/predProject7/Application.java
@@ -1,4 +1,4 @@
-package ru.javamentor.controllers;
+package ru.javamentor.predProject7;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/main/java/ru/javamentor/predProject7/controllers/GreetingController.java
+++ src/main/java/ru/javamentor/predProject7/controllers/GreetingController.java
@@ -1,4 +1,4 @@
-package ru.javamentor.controllers;
+package ru.javamentor.predProject7.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -7,9 +7,10 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class GreetingController {
-    @GetMapping("/greeting")
-    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+    @GetMapping("/index")
+    public String greeting(@RequestParam(name = "name", required = false, defaultValue = "World")String name, Model model) {
         model.addAttribute("name", name);
-        return "greeting";
+
+        return "index";
     }
 }
