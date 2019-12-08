236a5100113fb9e1e9f24ff381618a2ac02a62b4
nick <gnn@mm.st>
2019-12-08 16:59:26 +0300

    add access rights

--- src/main/java/ru/javamentor/predProject7/config/WebSecurityConfig.java
+++ src/main/java/ru/javamentor/predProject7/config/WebSecurityConfig.java
@@ -25,6 +25,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         http
                 .authorizeRequests()
                     .antMatchers("/", "/registration").permitAll()
+                    .antMatchers("/all").hasRole("ADMIN")
                     .anyRequest().authenticated()
                 .and()
                     .formLogin()
--- src/main/java/ru/javamentor/predProject7/controllers/GetAllUsersController.java
+++ src/main/java/ru/javamentor/predProject7/controllers/GetAllUsersController.java
@@ -1,6 +1,7 @@
 package ru.javamentor.predProject7.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
